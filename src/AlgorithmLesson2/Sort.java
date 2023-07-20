9a03ce653586be65c1d396e4f7b0f3b6f891e440
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-07-20 22:45:43 +0300

    Add directSort

--- src/AlgorithmLesson2/Sort.java
+++ src/AlgorithmLesson2/Sort.java
@@ -6,7 +6,7 @@ public class Sort {
         int[] array = new int[] {
                 4, 2, 5, 7, 6, 8, 1, -7, 3, 9
         };
-        bubbleSort(array);
+        dirertSort(array);
 
         for (int i = 0; i < array.length; i++){
             System.out.print(array[i] +  " ");
@@ -27,5 +27,21 @@ public class Sort {
         }
 
         } while (!finish);
+    }
+
+    public static void dirertSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minPosithon = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] <  array[minPosithon]) {
+                    minPosithon = j;
+                }
+            }
+            if (i != minPosithon) {
+                int temp = array[i];
+                array[i] = array[minPosithon];
+                array[minPosithon] = temp;
+            }
+        }
     }
 }
